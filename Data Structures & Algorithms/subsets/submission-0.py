baa8b1ac5aaaabaa9fbbb1bdbaa424122d769f56
nguyntinkhang <116292903+karuufumi@users.noreply.github.com>
2026-08-04 17:26:43 +0700

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/subsets/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        tmp = []
+        def dfs(n):
+            if n >= len(nums):
+                res.append(tmp[:])
+                return
+            tmp.append(nums[n])
+            dfs(n+1)
+            tmp.pop()
+            dfs(n+1)
+
+        dfs(0)
+        return res
