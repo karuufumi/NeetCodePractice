a01ae7432dca28cf2dcb5604a3e1f1ebc97f8f60
Nguyễn Tiến Khang <116292903+karuufumi@users.noreply.github.com>
2026-08-31 22:00:55 +0700

    Add: binary-tree-inorder-traversal - submission-0

---
+++ Data Structures & Algorithms/binary-tree-inorder-traversal/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def inorderTraversal(self, root):
+        res = []
+
+        def dfs(node):
+            if node is None:
+                return
+
+            dfs(node.left)
+            res.append(node.val)
+            dfs(node.right)
+
+        dfs(root)
+
+        return res
