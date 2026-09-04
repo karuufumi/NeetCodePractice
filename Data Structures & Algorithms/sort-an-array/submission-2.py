fea65816dfe990f57b662def65484bad88d900bb
Nguyễn Tiến Khang <116292903+karuufumi@users.noreply.github.com>
2026-09-04 17:23:55 +0700

    Add: sort-an-array - submission-2

---
+++ Data Structures & Algorithms/sort-an-array/submission-2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        # Shellsort
+        def insSort(nums, n, incr):
+            i = incr
+
+            while i < n:
+                j = i
+
+                while j >= incr and nums[j] < nums[j - incr]:
+                    #swap(nums, j, j - incr)
+                    nums[j],nums[j-incr] = nums[j-incr],nums[j]
+                    j -= incr
+
+                i += 1
+
+        n = len(nums)
+        gap = n // 2
+
+        while gap > 0:
+            insSort(nums, n, gap)
+            gap //= 2
+
+        return nums
